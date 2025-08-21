66fa3b9baa6e2e45edc212596cd7529a48236540
Jordan Low <jordanlow.jinyi@gmail.com>
2025-08-21 15:21:19 +0800

    Add Dad.java

---
+++ src/main/java/Dad.java
@@ -0,0 +1,10 @@
+public class Dad {
+    public static void main(String[] args) {
+	System.out.println("-------------------");
+        System.out.println("Hello I'm Dad");
+	System.out.println("Whaddya want?");
+	System.out.println("-------------------");
+	System.out.println("'Kay I'm headin' out");
+	System.out.println("-------------------");
+    }
+}
