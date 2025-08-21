0da45d07c1e157fa09b0262b7216e1740f37db43
Jordan Low <jordanlow.jinyi@gmail.com>
2025-08-21 15:36:30 +0800

    Update Dad.java with Echo

--- src/main/java/Dad.java
+++ src/main/java/Dad.java
@@ -1,10 +1,26 @@
+import java.util.Scanner;
+
 public class Dad {
-    public static void main(String[] args) {
-	System.out.println("-------------------");
-        System.out.println("Hello I'm Dad");
-	System.out.println("Whaddya want?");
-	System.out.println("-------------------");
-	System.out.println("'Kay I'm headin' out");
-	System.out.println("-------------------");
-    }
+	public static void main(String[] args) {
+		System.out.println("  ----------------------------------");
+		System.out.println("	Hello I'm Dad");
+		System.out.println("	Whaddya want?");
+		System.out.println("  ----------------------------------\n");
+
+		Scanner scanner = new Scanner(System.in);
+		boolean sentinel = true;
+		do {
+			String command = scanner.nextLine();
+			if (command.toLowerCase().equals("bye")) {
+				sentinel = false;
+			} else {
+				System.out.println("  ----------------------------------");
+				System.out.println("	" + command);
+				System.out.println("  ----------------------------------\n");
+			}
+		} while (sentinel);
+		System.out.println("  ----------------------------------");
+		System.out.println("	'Kay I'm headin' out");
+		System.out.println("  ----------------------------------\n");
+	}
 }
