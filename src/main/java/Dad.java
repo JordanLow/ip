05e72028763060c94f1c9563b07fd21a4b083269
Jordan Low <jordanlow.jinyi@gmail.com>
2025-08-21 15:50:14 +0800

    Add listing to Dad.java

--- src/main/java/Dad.java
+++ src/main/java/Dad.java
@@ -1,4 +1,6 @@
 import java.util.Scanner;
+import java.util.List;
+import java.util.ArrayList;
 
 public class Dad {
 	public static void main(String[] args) {
@@ -6,6 +8,8 @@ public class Dad {
 		System.out.println("	Hello I'm Dad");
 		System.out.println("	Whaddya want?");
 		System.out.println("  ----------------------------------\n");
+		
+		List<String> list = new ArrayList<>();
 
 		Scanner scanner = new Scanner(System.in);
 		boolean sentinel = true;
@@ -13,10 +17,17 @@ public class Dad {
 			String command = scanner.nextLine();
 			if (command.toLowerCase().equals("bye")) {
 				sentinel = false;
+			} else if (command.toLowerCase().equals("list")) {
+				System.out.println("  ----------------------------------");	
+				for (int i = 0; i < list.size(); i++) {
+					System.out.println("	" + (i+1) + ": " + list.get(i));
+				}
+				System.out.println("  ----------------------------------\n");
 			} else {
 				System.out.println("  ----------------------------------");
-				System.out.println("	" + command);
+				System.out.println("	added: " + command);
 				System.out.println("  ----------------------------------\n");
+				list.add(command);
 			}
 		} while (sentinel);
 		System.out.println("  ----------------------------------");
